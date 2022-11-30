8ae895dac17b0f7585eea77a2749bc8f3f83cd62
Amar <amar1998mishra@gmail.com>
2022-11-30 16:28:38 +0530

    this is basic concept of JavaScript

---
+++ script.js
@@ -0,0 +1,873 @@
+// console.log("Hello Word");
+// // alert("This is alert")
+// var myName = "Amar Rajan";
+// console.log(myName);
+// console.log(typeof(myName));
+
+
+
+// var a = 20;
+// console.log(a);
+// console.log(typeof(a));
+
+// var b = "30";
+// console.log(b);
+// console.log(typeof(b));
+
+// var c;
+// console.log(c);
+// console.log(typeof(c));
+
+// var d = null;
+// console.log(d);
+// console.log(typeof(d));
+
+// var a = 10;
+// var b = 30;
+// console.log(a+b);
+
+// var c = 20;
+// var d = "30";
+// console.log(c+d);
+
+// var myFirstName = "Amar";
+// var myLastName = "Mishra";
+// console.log(myFirstName+myLastName);
+
+// var a = 50;
+// var b = "10";
+// console.log(a-b);
+// var a = 10;
+// console.log(a);
+// console.log(typeof(a));
+
+// Data Type Primitive
+// Undefine
+// Boolean
+// Number
+// String
+// BigInt
+// Symbol
+
+
+// var a = 2.4;
+// console.log(a);
+// // typeof
+// console.log(typeof(a));
+
+// var myName = "100";
+// console.log(myName);
+// console.log(typeof(myName));
+
+
+// var a;
+// console.log(a);
+// console.log(typeof(a));
+
+// var a = null;
+// console.log(a);
+// console.log(typeof(a));
+// var a = true;
+// console.log(a);
+// console.log(typeof(a));
+
+// var myName = prompt("Enter Your Name");
+// console.log(myName);
+// var a = prompt("Enter a Number");
+// console.log(a);
+// console.log(typeof(a));
+
+// var a = Number(prompt("Enter a Number"));
+// console.log(a);
+// console.log(typeof(a));
+
+// Operator
+// Arithmetic Operator
+// +
+// -
+// *
+// /
+// %
+// Increment and Decrement Operator
+// var a = 2, b = 3;
+//  a++;
+//  ++a;
+//  --a;
+//  a--;
+// console.log(a+b);
+// console.log(a**b);
+// console.log(b/a);
+// console.log(b%a);
+
+// var a = 10;
+// a++;
+// console.log(a);
+// var a = 10;
+// ++a;
+// console.log(a++);
+// console.log(a);
+
+// console.log(--a);
+// console.log(a);
+
+// var a = 30, b =20;
+// var c = a + b;
+// a = a + b;
+// a = b;
+// console.log(a);
+
+// var a = 10;
+
+
+// console.log(a != b);
+// console.log(a >= b);
+
+// console.log(a == b);
+// console.log(a === b);
+
+// Logical Operator
+
+// && Logical AND
+// || Logical OR
+// ! Logical NOT
+
+// var a = 20, b = 30, c = 20;
+// var d = a == b && a == c;
+// console.log(d);
+// var d = a == b || a == c;
+// console.log(d);
+// var d = !(a == b)
+// console.log(d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var myName = "Amar";
+// console.log(myName);
+// console.log(typeof(myName))
+
+// var num1 = 30;
+// var num2 = 20;
+// var total = num1 + num2;
+// console.log("Total of Two Number"+ " " + total);
+
+
+
+// var num1 = 30;
+// var num2 = 20;
+// var total = num1 - num2;
+// console.log("Total of Two Number"+ " " + total);
+// console.log(num1-num2);
+
+// var num1 = 30;
+// var num2 = 20;
+// var total = num1 * num2;
+// console.log("Total of Two Number"+ " " + total);
+
+// var num1 = 30;
+// var num2 = 20;
+// var total = num1 / num2;
+// console.log("Total of Two Number"+ " " + total);
+
+
+// var num1 = 35;
+// var num2 = 20;
+// var total = num1 % num2;
+// console.log("Total of Two Number"+ " " + total);
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 ** num2;
+// console.log("Total of Two Number"+ " " + total);
+
+
+// var num1 = 30;
+// var num2 = 20;
+
+// console.log(num2);
+
+// num2--;
+// console.log(num2);
+
+// Assignment Operator
+// Arithmetic Operator
+// Comparison Operator
+// Logical Operator
+// String Operator
+// Conditional Operator
+
+// var a = 10;
+// var b = 12;
+// console.log((a === b) ? "A is Equal to B": "Not Equal")
+// if(a === b){
+//     console.log("A is equal to B");
+// }else{
+//     console.log("Not Equal");
+// }
+
+
+// var a = 20;
+// var b = 30;
+// console.log(a>b);
+// var a = 20;
+// var b = 30;
+// console.log(a<b);
+
+// var a = 20;
+// var b = 30;
+// console.log(a <= b);
+
+// var a = 20;
+// var b = 20;
+// console.log(a != b);
+
+// var a = 20;
+// var b = 20;
+// console.log(a == b);
+
+// var a = 20;
+// var b = 30;
+// console.log(a != b);
+
+// var age = 10;
+// if(age > 18){
+//     console.log("You are eligible for Vote");
+// }else{
+//     console.log("Not Eligible");
+// }
+
+// var year = 2021;
+// if(year%4 == 0){
+//     if(year%100 != 0 ){
+//         console.log("This is Leap Year");
+//     }else{
+//         console.log("Not a Leap Year");
+//     }
+// }else{
+//     console.log("Not a Leap Year");
+// }
+
+// var a = 2000, b = 3000, c = 200;
+// if(a > b && a > c){
+//     console.log("A is the largest Number" + a);
+// }else if(b > a && b > c){
+//     console.log("B is the largest Number" + b);
+// }else if(c > a && c > b){
+//     console.log("C is the largest Number" + c);
+// }else if(a == b && b == c & c == a){
+//     console.log("All are equal");
+// }
+
+// var a = 10;
+// if(!a){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// var a = 100, b = 105;
+// console.log((a > b) ? "A is greater": "B is Greater");
+
+// if(a > b){
+//     console.log("A is greater");
+// }
+// else{
+//     console.log("B is greater");
+// }
+
+//  var a = 20, b = 30, c = 40;
+//  if(a > b){
+//     if(a > c){
+//         console.log("A is greater")
+//     }
+//  }
+
+
+
+
+// var myName = Number(prompt("Enter Name"));
+// console.log(typeof(myName));
+// var myName = prompt("Enter Name");
+// document.write(myName);
+// var Name = document.getElementById('hi');
+// Name.innerHTML = myName;
+
+
+// Control Statement
+// var age = 12;
+// if(age > 18){
+//     console.log("You are eligible for Vote");
+// }else{
+//     console.log("Not Eligible");
+// }
+
+// var num = 21;
+// if(num%2 == 0){
+//     console.log("Given number is Even");
+// }else{
+//     console.log("Given number is Odd");
+// }
+// a, b, c,
+
+// var a = Number(prompt("Enter First Number"));
+// var b = Number(prompt("Enter 2nd Number"));
+// var c = Number(prompt("Enter 3rd Number"));
+
+// if(a > b &&  a > c){
+//     console.log("a is greater");
+    
+// }else if(b > a && b > c){
+//     console.log("B is Greater");
+// }else if(c > b && c > a){
+//     console.log("C is Greater");
+// }else if( a ==b && b == c && c == a){
+//     console.log("All are Equal");
+// }else{
+//     console.log("Invalid")
+// }
+// var a = 200;
+// var b = 30;
+// console.log((a>b) ? "Condition is True" : "Condtion is false");
+
+// Switch Case
+
+// switch(var){
+//     case 1:
+//         Statement
+//     case 2 :
+//         Statement
+//     case 3 :
+//         Statement
+//     default : 
+//         Statement
+// }
+
+// var a = Number(prompt("Enter Number"));
+// switch(a){
+//     case 1:
+//         console.log("Mon");
+//         break;
+//     case 2:
+//         console.log("Tue");
+//         break;
+//     case 3:
+//         console.log("wed");
+//         break;
+//     case 4:
+//         console.log("Thur");
+//         break;
+//     case 5:
+//         console.log("Fry");
+//         break;
+//     case 6:
+//         console.log("Sat");
+//         break;
+//     case 7:
+//         console.log("Sun");
+//         break;
+//     default:
+//         console.log("Invalid")
+// }
+
+// var a = 20, b = 30, c= 30;
+// ((a>b) ? 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Control Statements
+
+//if. Else Syntax(if(cond){
+    //block of code
+//})
+
+
+// if(cond){
+
+// }
+// else{
+
+// }
+
+// if(con1){
+
+// }
+// else if(cond2){
+
+// }
+// else{
+
+// }
+
+
+
+
+// Switch
+// var day = 3;
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tue");
+//         break;
+//     case 3:
+//         console.log("Wed");
+//         break;
+//     case 4:
+//         console.log("Thur");
+//         break;
+//     default:
+//         console.log("Invalid input");
+// }
+
+
+
+// var a = Number(prompt("Enter Input"));
+// console.log(a);
+// console.log(typeof(a));
+
+// var a = 10;
+// var b = "10";
+// if(a === b){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+
+// Loop
+// while(condition){
+//     expression
+// }
+// var counter = 1;
+// while(counter <= 100){
+//     console.log(counter);
+//     counter++;
+// }
+// var a = "No"
+// do{
+//     console.log("You can go and Talk to Counsler")
+// }while(a === "Yes");
+
+// var counter = 1;
+// do{
+//     console.log(counter);
+//     counter++;
+// }while(counter <= 10);
+
+// for(var a = 1; a <= 10; a++){
+//     console.log(a);
+// }
+
+
+// switch(exp){
+    // case x:
+        //code
+// break;
+// case y:
+    //code
+    // break;
+    // default:
+        //code
+// }
+
+
+
+
+
+
+//Loops
+
+// console.log("Starting of the Loop")
+// while(a <= 10){
+//     // console.log("Hello word \n");
+//     document.write("Hello Word", "<br/>")
+//     a++;
+//     // a += 1;
+// }
+// console.log("Ending Loop");
+// var a = 11;
+
+// do{
+//     document.write("Hello word","<br/>")
+//     a++;
+// }while(a <= 10); //Exit Control Loop
+
+
+// for(var a = 1; a <= 10; a++){
+//     document.write("Hello Welcome To JavaScript For Loop", "<br/>")
+    
+// }
+// var a = Number(prompt("Enter Your Number"));
+// for(var i = 1; i <= 10; i++){
+//     console.log(a*i);
+// }
+
+// Even odd
+// for(var i = 1; i <= 100; i++){
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for
+
+// for(let i=0;i<5;i++){
+//code
+// }
+
+
+//For In
+// const n=[4,6,7];
+// let txt="";
+// for(let x in n){
+//     txt+=n[x];
+// }
+
+//for each
+// const n=[4,6,7];
+// let txt="";
+// n.forEach(myfunc);
+// function myfunc(value,index,array)
+// {
+//     txt+=value;
+// }
+
+//while(entry control loop)
+
+//while(i<10){
+    //code
+ //   i++;
+//}
+
+//do while(exit control loop)
+
+// do{
+    //code
+    // i++
+// }
+// while(cond);
+
+//break
+//continue
+
+// Type Conversion
+// var a = prompt("Enter a Number"); //45
+// console.log(a);
+// console.log(typeof(a))
+// For the Number
+// Number()
+// var a = Number(prompt("Enter a Number")); //45
+// console.log(a);
+// console.log(typeof(a))
+
+// parseInt()
+// var a = parseInt(prompt("Enter a Number")); //45
+// console.log(a);
+// console.log(typeof(a))
+
+// parseFloat();
+// var a = parseFloat(prompt("Enter a Number")); //45
+// console.log(a);
+// console.log(typeof(a))
+// var a = '10';
+// console.log(a);
+// console.log(typeof(a));
+// console.log(Number(a));
+// a.Number()
+
+// For the String
+// var a = 10;
+// console.log(typeof(a));
+
+// console.log(String(a))
+// var b = String(a);
+// var b = a.toString();
+// var b = a.String()
+// console.log(typeof(b));
+// toString()
+
+// Function
+// function functionName(){
+//     // block of Code
+// }
+// functionName()
+
+// function wishMe(){
+//     // console.log("Welcome to JavaScript")
+//     var a = 10, b = 20;
+//     console.log(a+b);
+// }
+//  wishMe();
+
+// Function Type
+// function without parameter and without returning Value
+// function wishHi(){
+//     document.write("Hii", "<br/>");
+// }
+// wishHi();
+// function with parameter and without returning Value
+// function wishBdy(Name){
+//     console.log("Happy Birthday " + Name)
+// }
+// wishBdy("Rajan");
+// wishBdy("Amar");
+
+// function addNum(num1, num2){
+//     console.log(num1+num2);
+// }
+// var a = Number(prompt("Enter Num 1"))
+// var b = Number(prompt("enter num 2"))
+// addNum(20, 30)
+// addNum(30, 60)
+// addNum(a, b)
+
+// function without parameter and with returning Value
+
+// function addNum(){
+//     var a = 30, b = 60;
+//     return (a+b)
+// }
+// var sum = addNum();
+// console.log(sum);
+// console.log(addNum());
+
+// function with parameter and with returning Value
+
+// function addNum(num1, num2){
+//     return (num1+num2);
+// }
+// console.log(addNum(20, 30));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var  a = prompt("Enter Your Number : ");
+// console.log(a);
+// var b = prompt("Enter Second Number : ");
+// console.log(b);
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(a+b);
+
+// var a = Number(prompt("Enter Your First Number"));
+// var b = Number(prompt("Enter Your Second Number"));
+// console.log(a + b);
+// console.log(typeof(a));
+// var a = parseFloat(prompt("Enter Your First Number"));
+// var b = Number(prompt("Enter Your Second Number"));
+// console.log(a);
+// console.log(b);
+// console.log(typeof(a));
+
+// var a = 20;
+// console.log(a);
+// console.log(typeof(a));
+// var c = String(a);
+// var d = a.toString();
+// // console.log();
+// console.log(typeof(d));
+
+// var a = [2, 3, 5, 20, 30];
+// console.log(a);
+// console.log(a[3]);
+
+// var b = {
+//     Name : "Amar",
+//     Age : 27,
+//     Gender : "Male"
+// }
+// console.log(b.Name);
+
+// var counter = document.querySelector('.counter');
+// let count = 0;
+// function handleFun(button){
+//     if(button == plus){
+//         count += 1;
+//     }else if(button == minus){
+//         count -= 1;
+//     }else if(button == reset){
+//         count = 0;
+//     }
+//     counter.innerHTML = count;
+// }
+
+
+
+// Function
+// Type of JavaScript Function
+// 1. A function without parameter and without returning value
+// 2. A function without parameter and with returning value
+// 3. A function with parameter and without returning value
+// 4. A function with parameter and with returning value
+// function addNum(){
+//     var a = 10, b = 20;
+//     return (a+b);
+// }
+// //  var c = addNum();
+//  console.log(addNum());
+// function addNum(myName){
+//     document.write(myName);
+//     console.log(myName);
+    
+// }
+// addNum("Amar");
+// function wishMe(myName){
+//     return (myName);
+
+// }
+// var a = wishMe("Amar");
+// console.log(a);
+// var b = wishMe("Rajan");
+// console.log(b);
+// var a = 30;
+// function num(){
+    
+//     console.log(a);
+// }
+// num();
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+// function wishHi(){
+//     document.write("Hello Good Morning", " Hello Good Evening", "<br/>");
+// }
+// wishHi();
+
+// function wishMe(name){
+//     document.write("Hello ", name);
+// }
+// wishMe("Amar");
+// function Add(b){
+//     document.write(b);
+// }
+
+// var a = Number(prompt("Enter num"));
+// Add(a);
+
+// var counter = document.querySelector('.counter');
+//        const count = 0;
+//        function handleFun(button){
+//         if(button== plus){
+//             count += 1;
+//         }else if(button == minus){
+//             if(count > 0) {
+//                 count -= 1;
+//             }
+//         }else if(button == reset){
+//             count = 0;
+//         }
+//         counter.innerHTML = count;
+//        }
+// var a = 10;
+// console.log(a);
+// var a = 20;
+// console.log(a);
+// let a = 20;
+// console.log(a);
+// a = 30;
+// console.log(a);
+
+// const a = 30;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+
+// Factorial Program
+
+// let a = Number(prompt("Enter the number "));
+// var fact = 1;
+
+// for(var i = a; i >= 1; i--){
+//     fact = fact*i;
+// }
+// console.log(fact);
+
+// WAP to print Even number upto 100
+
+// var a = 1;
+// for(var a = 1; a <= 100; a++){
+//     if(a%2 == 0){
+//         console.log(a);
+//     }
+
+// }
+
+// var a = Number(prompt("Enter a number "));
+// for(var i = 1; i <=10; i++ ){
+//     console.log(i * a);
+// }
+
